87719f7c2c644737135d3069ced3debf16d1928c
maxwen <max.weninger@gmail.com>
2024-11-03 15:39:10 +0100

    raven: update build fingerprint AP3A.241005.015
    Change-Id: Ibe2d5a1ef21be332ff87a8fc981d29f6d8abf749

--- omni_raven.mk
+++ omni_raven.mk
@@ -31,10 +31,10 @@ PRODUCT_BRAND := google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=oriole \
-    PRIVATE_BUILD_DESC="raven-user 14 AP2A.240605.024 11860263 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 15 AP3A.241005.015 12366759 release-keys"
 
 # defined in vendor/google_devices/raven/proprietary/device-vendor.mk
-BUILD_FINGERPRINT := google/raven/raven:14/AP2A.240605.024/11860263:user/release-keys
+BUILD_FINGERPRINT := google/raven/raven:15/AP3A.241005.015/12366759:user/release-keys
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
