12eedeec9c7af77f8d436f8168e95ebc31e54030
maxwen <max.weninger@gmail.com>
2024-11-20 13:36:31 +0100

    oriole: fix the adaption to the new product override scheme
    Change-Id: I1394c0baa7e0699b70f3ffb3d832d6a4f85c4c1f

--- omni_oriole.mk
+++ omni_oriole.mk
@@ -30,7 +30,7 @@ PRODUCT_MODEL := Pixel 6
 PRODUCT_BRAND := google
 
 PRODUCT_BUILD_PROP_OVERRIDES := \
-    BuildDesc=oriole-user[[:space:]]15[[:space:]]AP3A.241005.015[[:space:]]12366759[[:space:]]release-keys \
+    BuildDesc="oriole-user 15 AP3A.241005.015 12366759 release-keys" \
     BuildFingerprint=google/oriole/oriole:15/AP3A.241005.015/12366759:user/release-keys \
     DeviceProduct=oriole
 
